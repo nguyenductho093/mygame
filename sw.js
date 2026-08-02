db9a3b048131800b1da82bba53b9ad1f80377f8e
nguyenductho093 <nguyenductho093@gmail.com>
2026-08-02 10:52:51 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('PWA Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Giúp trang web hoạt động mượt mà
+});
+
